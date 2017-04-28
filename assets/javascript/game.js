a4ee729492ad0534b6185a0e62d837a7b564b353
RaeVaughan <rachelev.tx@gmail.com>
2017-04-28 10:54:20 -0500

    add html structure and win and lose loops

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,25 +2,42 @@
 
 var answers = ["knope", "perkins", "swanson", "pawnee", "eagleton", "sweetums", "raccoon", "galentines", "gryzzlbox", "waffles"];
 var word = answers[Math.floor(Math.random() * answers.length)];
-var blanks = word.split("").map(function(){
-	return "_";
-});
-var guess = "";
-var guessCount = 12;
-var index = word.indexOf(guess);
+var blanks = word.split("").map(function(){return "_";});
+var guessCount = 10;
+var guessesMade = [];
+var wins = [];
 
-while(index > -1){
-	blanks[index] = guess;
-	var index = word.indexOf(guess, index + 1);
-}
+document.getElementById("word-display").innerHTML = "text here";
+//var blanksDisplay = document.createElement("div");
+//blanksDisplay.innerHTML = "blanks";
+//wordDisplay.appendChild(blanksDisplay);
 
-//document.onkeyup = function(event){
-//  var key = String.fromCharCode(event.keyCode).toLowerCase();
-//	if (){
-//		console.log("true");
-//	}
-//};
 
+document.onkeyup = function(event){
+
+  var guess = (event.key).toLowerCase();
+ 	//console.log(guess);
+ 	var index = word.indexOf(guess);
+ 	
+  while(index > -1){
+  	blanks[index] = guess;
+  	var index = word.indexOf(guess, index + 1);
+	};
+
+	if (index === -1){
+		guessCount--;
+		guessesMade.push(guess);
+	};
+
+
+
+};
+
+//to win
+//if (blanks.indexOf("_") === -1){};
+
+//to lose
+//if (guessCount === 0){};
 
 
 
