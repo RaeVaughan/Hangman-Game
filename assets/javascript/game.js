8fe5a4de740b2dd1937a84ec309c73988f683ff1
RaeVaughan <rachelev.tx@gmail.com>
2017-04-29 19:05:55 -0500

    game now recognizes only letters and the enter key (issue) & added images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,27 @@
 //Global variables
 
-var answers = ["knope", "perkins", "swanson", "pawnee", "eagleton", "sweetums", "raccoon", "galentines", "gryzzlbox", "waffles"];
+var answers = ["knope", "perkins", "swanson", "mouserat", "eagleton", "pawnee", "sweetums", "april", "gryzzlbox", "waffles"];
 var word = answers[Math.floor(Math.random() * answers.length)];
 var blanks = word.split("").map(function(){return "_";});
 var guessCount = 10;
 var guessesMade = [];
 var wins = 0;
+var regex = /^[a-zA-Z]*$/;
 
 document.getElementById("win-display").innerHTML = wins;
 document.getElementById("word-display").innerHTML = blanks;
 document.getElementById("remaining-guesses").innerHTML = guessCount;
+document.getElementById("image").src = "assets/images/pawnee-welcome.jpg";
 
 
-document.onkeyup = function(event){
 
+document.onkeypress = function(event){
   var guess = (event.key).toLowerCase();
  	//console.log(guess);
  	var index = word.indexOf(guess);
+
+ 	if (guess.match(regex)){
+
 
 	if(index === -1){
 		guessCount--;
@@ -38,28 +43,66 @@ document.onkeyup = function(event){
 
 //when you win
 	if(blanks.indexOf("_") === -1){
+
+		if(word === "knope"){
+			document.getElementById("defaultImg").src = "assets/images/knope.jpg"
+		}
+
+		if(word === "perkins"){
+			document.getElementById("defaultImg").src = "assets/images/perkins.jpg"
+		}
+
+		if(word === "swanson"){
+			document.getElementById("defaultImg").src = "assets/images/swanson.jpg"
+		}
+
+		if(word === "mouserat"){
+			document.getElementById("defaultImg").src = "assets/images/mouserat.jpg"
+		}
+
+		if(word === "eagleton"){
+			document.getElementById("defaultImg").src = "assets/images/eagleton.png"
+		}
+
+		if(word === "pawnee"){
+			document.getElementById("defaultImg").src = "assets/images/pawnee-welcome.jpg"
+		}
+
+		if(word === "sweetums"){
+			document.getElementById("defaultImg").src = "assets/images/sweetums.jpeg"
+		}
+
+		if(word === "april"){
+			document.getElementById("defaultImg").src = "assets/images/april.png"
+		}
+
+		if(word === "gryzzlbox"){
+			document.getElementById("defaultImg").src = "assets/images/gryzzl.jpg"
+		}
+
+		if(word === "waffles"){
+			document.getElementById("defaultImg").src = "assets/images/waffles.png"
+		}
+
 		wins++;
 		document.getElementById("win-display").innerHTML = wins;
+
 		guessCount=10;
 		document.getElementById("remaining-guesses").innerHTML = guessCount;
+
 		word = answers[Math.floor(Math.random() * answers.length)];
 		blanks = word.split("").map(function(){return "_";});
 		document.getElementById("word-display").innerHTML = blanks;
+
 	 guessesMade = [];
 	 document.getElementById("letters-guessed").innerHTML = guessesMade;
 	};
 
-
+}
 
 	
 
 
 
 };
-
-//to win
-//if (blanks.indexOf("_") === -1){};
-
-
-
 
